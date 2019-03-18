d84b0432cb3a4cf691d9273eabcd9f25c9e68fb4
Devansh Kaloti <034189@hsc.on.ca>
2019-03-18 13:24:44 -0400

    Fixed Bug on Windows

---
+++ convert.py
@@ -0,0 +1,114 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = "Devansh Kaloti"
+__copyright__ = "Copyright 2018, Convert"
+__credits__ = ["Devansh Kaloti", "Will Assad"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "DKSources"
+__email__ = "devansh@dksources.com"
+__status__ = "Production"
+
+"""convert.py will convert Intellij Projects
+   to be Netbeans compatible.
+"""
+import shutil, sys, os
+
+homedir = os.path.expanduser('~')
+pathToPath = os.path.join(homedir, "path.txt")
+
+try: #attempt to read the users path from path.txt
+    with open(pathToPath,"r") as f:
+        customPath = f.read()
+except FileNotFoundError: #if the user has run install
+    print("ERROR: Must run install.py")
+    quit()
+
+#get the correct file path depending on where this file is located
+def filepath(folder, filename):
+    return customPath + "/" + folder + "/" + filename
+
+#returns an instance of a file if no errors occur
+def openFile(file, permission):
+    try:
+        return open(file, permission)
+    except FileNotFoundError as e:
+        print("** File Not Found %s**" %file)
+    except PermissionError:
+        print("** Insufficient permissions to open file %s**\n\n" %file)
+    except Exception:
+        print("** Unknown Error While Opening File:  %s**\n\n" %file)
+
+    quit()
+
+# Reset the output folder to template.
+def resetFolder():
+    with openFile(filepath("template","project.xml"), "r") as templateProjectXML:
+        with openFile(filepath("nbproject","project.xml"), "w") as outputProjectXML:
+            outputProjectXML.write(templateProjectXML.read())
+
+    with openFile(filepath("template","project.properties"), "r") as templateProperties:
+        with openFile(filepath("nbproject","project.properties"), "w") as outputProperties:
+            outputProperties.write(templateProperties.read())
+
+
+# Coping contents of src folder to final folder
+def copyFolder(src, dist):
+    print(src, dist)
+    try:
+        shutil.copytree(src, dist)  # Copy files
+    except FileExistsError:
+        print("File Existed")
+    except Exception as e:
+        print(e)
+
+def copyFile(src, dist):
+    print(src, dist)
+    try:
+        shutil.copyfile(src, dist)
+    except FileExistsError:
+        print("File Existed")
+    except Exception as e:
+        print(e)
+
+# Get the project name, by finding last in pathname
+def getProjectName(path):
+    return str(path).split("/")[path.count("/")]
+
+
+# Replace the Project Name In XML file
+def replaceProjectName(srcPath):
+    #global srcPath
+    file = openFile(filepath("nbproject","project.xml"), "r")
+    lines = file.readlines()
+    file.close()
+
+    for i,line in enumerate(lines):
+        if "<name>******************</name>" in line:
+            lines[i] = "<name>%s</name>\n" %getProjectName(srcPath)
+
+
+    with openFile(filepath("nbproject","project.xml"), "w") as file:
+        file.write(''.join(lines))
+
+
+#main logic, convert project to NetBeans
+def main():
+    try: file_name = sys.argv[1]
+    except IndexError:
+        print("ERROR: Expected 1 Argument Containing File Path to convert ex. 'convert ...'")
+        return
+
+    srcPath = file_name.strip(" ")
+
+    replaceProjectName(srcPath)
+    copyFolder(filepath("nbproject",""), srcPath + "/nbproject")  # Copy settings
+    copyFile(filepath("template", "nbtointellij.iml"), srcPath + "/" + getProjectName(srcPath) + ".iml")
+
+    resetFolder()
+    print("\n\nYour project has been converted!\nPlease look in your source directory.")
+
+
+if __name__ == '__main__':
+    main()
--- install.py
+++ install.py
@@ -10,9 +10,18 @@ __maintainer__ = "DKSources"
 __email__ = "willassadcode@gmail.com"
 __status__ = "Production"
 
-import os, platform, sys
+import os, platform, sys, ctypes
+
+# Check if file is being run by admin
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 class InstallConvert():
+
+
 
     def __init__(self):
         print('+------------------------------------------------------+')
@@ -23,32 +32,38 @@ class InstallConvert():
         self.homedir = os.path.expanduser('~')
 
 
-    def setup(self):
 
+    def setup(self):
         if platform.system() == "Darwin":
             self.mac_osx_install_route()
         if platform.system() == "Windows":
-            self.windows_install_route()
+            if not is_admin():
+                print("Please run install.py with admin privileges")
+                print("You can run it as 'runas /user:{ADMINUSER} py install.py'")
+                quit(print("Exiting, Code 0"))
+            else:
+                self.windows_install_route()
 
         with open(os.path.join(os.path.expanduser('~'), "path.txt"),"w") as f:
             f.write(os.getcwd())
 
-
     def mac_osx_install_route(self):
         # spdir = os.path.join(self.homedir, "DKsources", "convert")
-        # os.system("chmod +x ./main.py")
+        # os.system("chmod +x ./convert.py")
         # os.system("mkdir " + spdir)
         # os.system('export PATH="$PATH:$HOME/bin"')
         # os.system("cp -r "+ self.homedir + spdir)
 
-        os.system("chmod +x ./main.py")
+        os.system("chmod +x ./convert.py")
         os.system('export PATH="$PATH:$HOME/bin"')
-        os.system("ln -s " + os.getcwd() + "/main.py /usr/local/bin/convert")
+        os.system("ln -s " + os.getcwd() + "/convert.py /usr/local/bin/convert")
 
     def windows_install_route(self):
         os.system("mkdir " + os.path.join(self.homedir, "DKsources", "convert"))
         os.system("xcopy \"" + os.getcwd() + "\" \"" + os.path.join(self.homedir, "DKsources", "convert") + "\" /s /y")
-        os.system("SETX PATH \"%PATH%;" + os.path.join(self.homedir, "DKsources") + "\"")
+        os.system("SET PATH \"%PATH%;" + os.path.join(self.homedir, "DKsources") + "\\")
+        os.system("SETX PATH \"%PATH%;" + os.path.join(self.homedir, "DKsources") + "\\")
+
 
 if __name__ == "__main__":
     installer = InstallConvert()
